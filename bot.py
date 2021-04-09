b686cce68de9d3623c680a85242efcf47efe0166
d3jott <djpromilly@gmail.com>
2021-04-09 23:54:37 +0200

    Changed on_ready method

--- bot.py
+++ bot.py
@@ -3,7 +3,13 @@ import discord
 
 @bot.event
 async def on_ready():
-    print("Bot has been enabled")
+    await bot.change_presence(activity=discord.Game(name=config["status"]))
+
+    print("   ___                 ____        _   \n"
+          "  / _ \__   _____ _ __| __ )  ___ | |_ \n"
+          " | | | \ \ / / _ \ '__|  _ \ / _ \| __|\n"
+          " | |_| |\ V /  __/ |  | |_) | (_) | |_ \n"
+          "  \___/  \_/ \___|_|  |____/ \___/ \__|")
 
 
 bot.run("")
