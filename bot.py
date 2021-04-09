ec588526664ea2ba5e2038759d051d4857a9f003
d3jott <djpromilly@gmail.com>
2021-04-09 23:58:14 +0200

    Added test command

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 import json
 
 import discord
+from discord.ext import commands
 
 with open('config/config.json', 'r') as cjson:
     config = json.load(cjson)
 
 bot = commands.Bot(command_prefix=config["prefix"])
+bot.remove_command('help')
 
 png = config["img"]
+
 
 @bot.event
 async def on_ready():
@@ -18,6 +21,11 @@ async def on_ready():
           " | | | \ \ / / _ \ '__|  _ \ / _ \| __|\n"
           " | |_| |\ V /  __/ |  | |_) | (_) | |_ \n"
           "  \___/  \_/ \___|_|  |____/ \___/ \__|")
+
+
+@bot.command(name='test')
+async def test(ctx):
+    await ctx.message.delete()
 
 
 bot.run(config["token"])
